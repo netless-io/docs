a28fadc172b17180537c7f8f0b79d83cbdd929e9
leavesster <a412739861@qq.com>
2019-05-23 20:18:31 +0800

    always show nav

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -118,7 +118,7 @@ const siteConfig = {
 
   // Show documentation's last update time.
   enableUpdateTime: true,
-  docsSideNavCollapsible: true,
+  docsSideNavCollapsible: false,
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
