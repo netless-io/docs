cd9906a5607b10e82a140c7893a4235b0849ca03
leavesster <a412739861@qq.com>
2019-05-25 11:10:36 +0800

    support platform

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -98,6 +98,7 @@ const siteConfig = {
     'https://buttons.github.io/buttons.js',
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
     '/js/code-block-buttons.js',
+    "/js/spec-code.js"
   ],
   stylesheets: ['/css/code-block-buttons.css'],
   // On page navigation for the current documentation page.
---
+++ website/static/js/spec-code.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const platform = urlParams.get('platform').toLowerCase();
+    let i = ["ios", "web", "android"].indexOf(platform);
+    console.log(`spec-code`, platform, i);
+    if (platform && i != -1) {
+        changeTabActive(platform, "nav-link");
+        changePanActive("tab-pane", i);
+    }
+});
+
+function changeTabActive(platform, className) {
+    const list = document.getElementsByClassName(className);
+    [].forEach.call(list, element => {
+        if (element.textContent.toLowerCase().indexOf(platform) != -1) {
+            element.className = `${className} active`;
+        } else {
+            element.className = `${className}`;
+        }
+    });
+}
+
+function changePanActive(className, i, total=3) {
+    const list = document.getElementsByClassName(className);
+    [].forEach.call(list, (element, index) => {
+        if (index % total == i) {
+            element.className = `${className} active`;
+        } else {
+            element.className = `${className}`;
+        }
+    });
+}
