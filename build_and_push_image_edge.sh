6a5ccd524b52b6b6e131cc1770475a45e244b7b8
buhe <buhe.bh@antfin.com>
2019-05-27 10:58:20 +0800

    add CI script for auto deploy k8s

---
+++ build_and_push_image_edge.sh
@@ -0,0 +1,14 @@
+set -exo pipefail
+
+image=netless-developer-docs
+version=1.0.0
+
+hash=$(git rev-parse --short HEAD)
+
+docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash -t registry.cn-hangzhou.aliyuncs.com/white/$image:latest .
+docker push registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash
+docker push registry.cn-hangzhou.aliyuncs.com/white/$image:latest
+
+ssh app@k8s-cloud -tt "cd /home/app/k8s-stack/site/netless-developer-docs && \
+    kubectl apply -f deploy.yml && \
+    kubectl patch deployment netless-developer-docs -n docs --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
