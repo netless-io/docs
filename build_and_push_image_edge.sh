3b83e39a8c834422dc5563eefc7cf8bbc459eda9
buhe <buhe.bh@antfin.com>
2019-05-27 11:07:08 +0800

    fix wrong ci script

--- build_and_push_image_edge.sh
+++ build_and_push_image_edge.sh
@@ -9,6 +9,6 @@ docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/white/$image:$ve
 docker push registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash
 docker push registry.cn-hangzhou.aliyuncs.com/white/$image:latest
 
-ssh app@k8s-cloud -tt "cd /home/app/k8s-stack/site/netless-developer-docs && \
+ssh app@k8s-cloud -tt "cd /home/app/k8s-stack/site/developer.herewhite.com && \
     kubectl apply -f deploy.yml && \
     kubectl patch deployment netless-developer-docs -n docs --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
