af511292ac98d9e55b31bcccd0877c6628fd91f2
leavesster <11785335+leavesster@users.noreply.github.com>
2019-07-18 14:07:41 +0800

    update code together

--- website/static/js/spec-code.js
+++ website/static/js/spec-code.js
@@ -1,12 +1,29 @@
+const platformList = ["web", "ios", "android"];
+
 window.addEventListener('load', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const platform = urlParams.get('platform');
-    let i = ["web", "ios", "android"].indexOf(platform ? platform.toLowerCase() : "");
-    if (platform && i != -1) {
-        changeTabActive(platform.toLowerCase(), "nav-link");
-        changePanActive("tab-pane", i);
+    let order = platformList.indexOf(platform ? platform.toLowerCase() : "");
+
+    if (platform && order != -1) {
+        updateStorage(platform);
+        updateCodeTab(platform, order);
+    } else {
+        const localPlatform = localStorage.getItem("platform");
+        if (localPlatform) {
+            updateCodeTab(localPlatform, platformList.indexOf(localPlatform));
+        }
     }
 });
+
+function updateStorage(platform) {
+    localStorage.setItem("platform", platform.toLowerCase());
+}
+
+function updateCodeTab(platform, order) {
+    changeTabActive(platform.toLowerCase(), "nav-link");
+    changePanActive("tab-pane", order);
+}
 
 function changeTabActive(platform, className) {
     const list = document.getElementsByClassName(className);
@@ -19,13 +36,28 @@ function changeTabActive(platform, className) {
     });
 }
 
-function changePanActive(className, order, total=3) {
+function changePanActive(className, order, total=platformList.length) {
     const list = document.getElementsByClassName(className);
     [].forEach.call(list, (element, index) => {
-        if (index % total == order) {
+        if (index % total == order % total) {
             element.classList.add("active");
         } else {
             element.classList.remove("active");
         }
     });
 }
+
+window.addEventListener('load', function() {
+    // add event listener for all tab
+    document.querySelectorAll('.nav-link').forEach(function(el) {
+      el.addEventListener('click', function(e) {
+        const groupId = e.target.getAttribute('id');
+        const result = groupId.match(/([\d]+)/g);
+        if (result && result.length >= 2) {
+            const order = parseInt(result[1]) - parseInt(result[0]) - 1;
+            updateCodeTab(platformList[order], order)
+            updateStorage(platformList[order]);
+        }
+      });
+    });
+  });
