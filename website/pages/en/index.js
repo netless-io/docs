f53dd7728930f025a5263146db765503d6fa4457
伍双 <beingswu@gmail.com>
2020-01-10 17:09:28 +0800

    translation homepage

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -19,16 +19,16 @@ class Index extends React.Component {
                             <img src={`${siteConfig.baseUrl}img/javascript.svg`}/>
                         </div>
                         <a href={`${siteConfig.baseUrl}docs/javascript/overview/js-outline`}>
-                            <div className="home-page-cell-inner">文档大纲</div>
+                            <div className="home-page-cell-inner">Overview</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/javascript/guide/js-sdk`}>
-                            <div className="home-page-cell-inner">安装指南</div>
+                            <div className="home-page-cell-inner">Installation guide</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/javascript/quick-start/js-precondition`}>
-                            <div className="home-page-cell-inner">快速开始</div>
+                            <div className="home-page-cell-inner">Quick start</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/javascript/features/js-tools`}>
-                            <div className="home-page-cell-inner">功能介绍</div>
+                            <div className="home-page-cell-inner">Features</div>
                         </a>
                     </div>
                     <div className="home-page-cell">
@@ -37,16 +37,16 @@ class Index extends React.Component {
                             <img src={`${siteConfig.baseUrl}img/android.svg`}/>
                         </div>
                         <a href={`${siteConfig.baseUrl}docs/android/overview/android-introduction`}>
-                            <div className="home-page-cell-inner">产品概述</div>
+                            <div className="home-page-cell-inner">Overview</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/android/quick-start/android-declare`}>
-                            <div className="home-page-cell-inner">快速开始</div>
+                            <div className="home-page-cell-inner">Quick start</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/android/guides/android-state`}>
-                            <div className="home-page-cell-inner">常用功能</div>
+                            <div className="home-page-cell-inner">Features</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/android/guides/android-v2migration`}>
-                            <div className="home-page-cell-inner">升级指南</div>
+                            <div className="home-page-cell-inner">Installation guide</div>
                         </a>
                     </div>
                     <div className="home-page-cell">
@@ -55,16 +55,16 @@ class Index extends React.Component {
                             <img src={`${siteConfig.baseUrl}img/ios.svg`}/>
                         </div>
                         <a href={`${siteConfig.baseUrl}docs/ios/overview/ios-introduction`}>
-                            <div className="home-page-cell-inner">产品概述</div>
+                            <div className="home-page-cell-inner">Overview</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/ios/quick-start/ios-declare`}>
-                            <div className="home-page-cell-inner">快速开始</div>
+                            <div className="home-page-cell-inner">Quick start</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/ios/guides/ios-state`}>
-                            <div className="home-page-cell-inner">常用功能</div>
+                            <div className="home-page-cell-inner">Features</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/ios/guides/ios-v2migration`}>
-                            <div className="home-page-cell-inner">升级指南</div>
+                            <div className="home-page-cell-inner">Upgrade guide</div>
                         </a>
                     </div>
                     <div className="home-page-cell">
@@ -73,28 +73,28 @@ class Index extends React.Component {
                             <img src={`${siteConfig.baseUrl}img/server.svg`}/>
                         </div>
                         <a href={`${siteConfig.baseUrl}docs/server/overview/server-introduction`}>
-                            <div className="home-page-cell-inner">产品概述</div>
+                            <div className="home-page-cell-inner">Overview</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/server/api/server-request`}>
-                            <div className="home-page-cell-inner">服务端 API</div>
+                            <div className="home-page-cell-inner">Server API</div>
                         </a>
                     </div>
                     <div className="home-page-cell">
-                        <div className="home-page-cell-title">博客</div>
+                        <div className="home-page-cell-title">Blog</div>
                         <div className="home-page-cell-icon">
                             <img src={`${siteConfig.baseUrl}img/blog.svg`} />
                         </div>
                         <a href={`${siteConfig.baseUrl}docs/blog/blog-add-driver`}>
-                            <div className="home-page-cell-inner">配置云存储</div>
+                            <div className="home-page-cell-inner">Cloud storage</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/blog/debug-center`}>
-                            <div className="home-page-cell-inner">在线 debug</div>
+                            <div className="home-page-cell-inner">online debug</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/blog/blog-begin-netless`}>
-                            <div className="home-page-cell-inner">使用说明</div>
+                            <div className="home-page-cell-inner">Instructions</div>
                         </a>
                         <a href={`${siteConfig.baseUrl}docs/blog/blog-broadcast`}>
-                            <div className="home-page-cell-inner">主播与观众</div>
+                            <div className="home-page-cell-inner">Screen control</div>
                         </a>
                     </div>
                 </div>
@@ -108,7 +108,7 @@ class Index extends React.Component {
                                 <div className="home-page-box-banner-cell-box">
                                     <img style={{width: 104, marginBottom: 12}} src={`${siteConfig.baseUrl}img/landing.svg`}/>
                                 </div>
-                                <div className="home-page-box-banner-cell-title">官方网站</div>
+                                <div className="home-page-box-banner-cell-title">Home page</div>
                             </div>
                         </a>
                         <a href="https://console.herewhite.com/" target="_blank">
@@ -116,7 +116,7 @@ class Index extends React.Component {
                                 <div className="home-page-box-banner-cell-box">
                                     <img style={{width: 108, marginBottom: 12}} src={`${siteConfig.baseUrl}img/console.svg`}/>
                                 </div>
-                                <div className="home-page-box-banner-cell-title">管理控制台</div>
+                                <div className="home-page-box-banner-cell-title">Console</div>
                             </div>
                         </a>
                         <a href="https://demo.herewhite.com/#/zh-CN/" target="_blank">
@@ -124,7 +124,7 @@ class Index extends React.Component {
                                 <div className="home-page-box-banner-cell-box">
                                     <img style={{width: 106, marginBottom: 12}} src={`${siteConfig.baseUrl}img/demo.svg`}/>
                                 </div>
-                                <div className="home-page-box-banner-cell-title">Demo 体验</div>
+                                <div className="home-page-box-banner-cell-title">Demo</div>
                             </div>
                         </a>
                     </div>
