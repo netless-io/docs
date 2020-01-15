42fc160acb145f6416fe82bbcdd1d865435b8cd3
leavesster <11785335+leavesster@users.noreply.github.com>
2020-01-15 12:00:59 +0800

    new image

--- build_and_push_image_edge.sh
+++ build_and_push_image_edge.sh
@@ -1,6 +1,7 @@
 set -exo pipefail
+# 构建 iamge，同时部署
 
-image=netless-developer-docs
+image=netless-developer-docs-en
 version=1.0.0
 
 hash=$(git rev-parse --short HEAD)
@@ -10,5 +11,5 @@ docker push registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash
 docker push registry.cn-hangzhou.aliyuncs.com/white/$image:latest
 
 ssh app@k8s-cloud -tt "cd /home/app/k8s-stack/site/developer.herewhite.com && \
-    kubectl apply -f deploy.yml && \
-    kubectl patch deployment netless-developer-docs -n site --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
+    kubectl apply -f docs-en.yml && \
+    kubectl patch deployment netless-developer-docs-en -n site --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
