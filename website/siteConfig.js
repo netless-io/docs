851933cc559832d8971203819c369bab5e9e507b
leavesster <11785335+leavesster@users.noreply.github.com>
2020-02-05 19:02:20 +0800

    fix link

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -42,7 +42,7 @@ const siteConfig = {
     {doc: 'android/overview/android-introduction', label: 'Android'},
     {doc: 'ios/overview/ios-introduction', label: 'iOS'},
     {doc: 'server/overview/server-introduction', label: 'Server'},
-    {doc: 'blog/blog-glossary', label: '博客'},
+    {doc: 'blog/term', label: '博客'},
     {
         href: 'https://github.com/netless-io',
         label: 'GitHub',
