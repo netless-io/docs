41c075370c9aded8e54924eb37077ac3d003c5c7
leavesster <11785335+leavesster@users.noreply.github.com>
2020-03-21 14:46:35 +0800

    isolation yaml just patch

--- build_and_push_image_edge.sh
+++ build_and_push_image_edge.sh
@@ -9,6 +9,4 @@ docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/white/$image:$ve
 docker push registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash
 docker push registry.cn-hangzhou.aliyuncs.com/white/$image:latest
 
-ssh k8s-site -tt "cd ~/k8s-site/site/developer.herewhite.com && \
-    kubectl apply -f deploy.yml && \
-    kubectl patch deployment netless-developer-docs -n site --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
+ssh k8s-site -tt "kubectl patch deployment docs -n site --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
