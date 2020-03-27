f2c78ebd4b77cc024a7a12a812bd38ccd4d9d239
leavesster <11785335+leavesster@users.noreply.github.com>
2020-03-27 13:09:36 +0800

    fix docs-en script

--- build_and_push_image_edge.sh
+++ build_and_push_image_edge.sh
@@ -1,6 +1,6 @@
 set -exo pipefail
 
-image=netless-developer-docs
+image=netless-developer-docs-en
 version=1.0.0
 
 hash=$(git rev-parse --short HEAD)
@@ -9,4 +9,4 @@ docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/white/$image:$ve
 docker push registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash
 docker push registry.cn-hangzhou.aliyuncs.com/white/$image:latest
 
-ssh k8s-site -tt "kubectl patch deployment docs -n site --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
+ssh k8s-site -tt "kubectl patch deployment docs-en -n site --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
